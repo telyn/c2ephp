851bbd11aed24b4f4421859e24137f9eb902da41
GameFreak7744 <GameFreak7744@c8db70c2-6952-0410-97aa-c93078ccbfcc>
2008-07-20 05:25:37 +0000

    organised the files slightly; separated hightlight.php up a bit; added some of the syntax so highlight.php can actually highlight CAOS (more still needs adding);

---
+++ caos/highlight/command_variables.php
@@ -0,0 +1,27 @@
+<?php
+$caosCommandVariables = array
+(
+//agents
+"pfft",
+"erk",
+"attr",
+"base",
+"bhvr",
+"gall",
+"hand",
+"mira",
+"plne",
+"pose",
+"puhl", //returns value and accepts arguments!
+"rnge",
+"show",
+"targ",
+"tick",
+"tint",
+"tran", //returns and accepts
+//cameras
+
+//variables
+"char"
+);
+?>
