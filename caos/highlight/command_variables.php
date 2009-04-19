9422046735190aacae80d13dce90761d428d4369
incarnadineai <incarnadineai@c8db70c2-6952-0410-97aa-c93078ccbfcc>
2009-04-19 19:24:45 +0000

    added some more categories of commands to caos highlighter

--- caos/highlight/command_variables.php
+++ caos/highlight/command_variables.php
@@ -1,27 +1,59 @@
 <?php
 $caosCommandVariables = array
 (
-//agents
-"accg",
-"attr",
-"base",
-"bhvr",
-"gall",
-"hand",
-"mira",
-"perm",
-"plne",
-"pose",
-"puhl", //returns value and accepts arguments!
-"rnge",
-"show",
-"targ",
-"tick",
-"tint",
-"tran", //returns and accepts
-//cameras
+	//agents
+	'accg',
+	'aero',
+	'elas',
+	'clac',
+	'attr',
+	'base',
+	'bhvr',
+	'gall',
+	'hand',
+	'mira',
+	'perm',
+	'plne',
+	'pose',
+	'puhl', //returns value and accepts arguments!
+	'rnge',
+	'show',
+	'targ',
+	'tick',
+	'tint',
+	'tran', //returns and accepts
+	//brain
 
-//variables
-"char"
+	//cameras
+	'bkgd',
+	'meta',
+	'snax',
+	'trck',
+	'wdow',
+	//CD Player
+	'__CD__',
+	//compounds
+	'page',
+	'part',
+
+	//creatures
+	'aslp',
+	'body',
+	'calg',
+	'chem',
+	'dead',
+	'dirn',
+	'drea',
+	'driv',
+	'face',
+	'loci',
+	'mind',
+	'motr',
+	'norn',
+	'soul',
+	'zomb',
+	
+	//variables
+	'char'
 );
 ?>
