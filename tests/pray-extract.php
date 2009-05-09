ff0dd60a56b57d042ec0735ff8d150449f8bffa6
Telyn Z <175827+telyn@users.noreply.github.com>
2009-05-09 20:39:46 +0000

    Everything works internally now, as far as I know.

--- tests/pray-extract.php
+++ tests/pray-extract.php
@@ -1,7 +1,7 @@
 <?php
 include(dirname(__FILE__).'/extract.php');
-$agent = new PRAYFile(file_get_contents('testcreature.creature'));
-$agent->Parse();
+
+
  
  ExtractCompleteAgent('testcreature.creature');
 ?>
