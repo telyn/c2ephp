045095bb67b83a8edf6e321550fae720a72854e3
miyanokouji.albion <miyanokouji.albion@c8db70c2-6952-0410-97aa-c93078ccbfcc>
2009-05-09 20:41:43 +0000

    Completed move of extract.php from pray/ to test/. Rationale: End users can write something to write the files out. We only need it to test, and eventually automatically turning S16/C16/SPR to PNG is going to be a supported feature.

---
+++ tests/extract.php
@@ -0,0 +1,68 @@
+<?php
+include('../support/FileReader.php');
+require_once('../pray/PRAYFile.php');
+require_once('../s16/S16File.php');
+ 
+//this function is pretty dirty, all things considered.
+ 
+function ExtractCompleteAgent($agent) {
+    $agent = new PRAYFile(new FileReader($agent));
+    $agent->Parse();
+    $blocks = $agent->GetBlocks();
+    $fileBlocks = $agent->GetBlocks('FILE');
+    foreach($blocks as $block) {
+        switch($block['Type']) {
+            case 'FILE':
+			case 'GENE':
+			case 'GLST':
+			case 'CREA':
+                $handle = fopen('./'.$block['Name'],'wb');
+                fwrite($handle,$block['Content']);
+                fclose($handle);
+                break;
+			case 'PHOT':
+				
+				$s16 = new S16File(new StringReader($block['Content']));
+				$png = fopen($block['Name'].'.png','wb');
+				fwrite($png,$s16->OutputPNG(0));
+				fclose($png);
+				break;                
+            case 'AGNT':
+            case 'DSAG':
+            case 'EGG': //after looking I don't think it needs any special treatment
+                $handle = fopen($block['Name'].'.'.$block['Type'].'.pray.txt','w');
+                fwrite($handle,'"en-GB"'."\r\n\r\n".'group '.$block['Type'].' "'.$block['Name']."\"\r\n");
+                foreach($block['Tags'] as $name=>$value) {
+                    
+                    if($name == "Script 1") { //since apparently there should only be one script ever (according to CDN)
+                        
+                        $cosFile = $block['Name'].'.'.$block['Type'].'.cos';
+                        $cosHandle = fopen('./'.$cosFile,'w');
+                        fwrite($cosHandle,$value);
+                        fclose($cosHandle);
+                        
+                        fwrite($handle,'"Script 1" @ "'.$cosFile."\"\r\n"); //special handling because the script directive is DUMB
+                    } else {
+                        if(is_int($value)) {
+                            fwrite($handle,'"'.$name.'" '.$value."\r\n");
+                        } else {
+                            $value = str_replace("\r","",$value);
+                            $value = str_replace("\n","",$value);
+                            fwrite($handle,'"'.$name.'" "'.$value."\"\r\n");
+                        }
+                    }
+                }
+                foreach($fileBlocks as $fileBlock) {
+                    fwrite($handle,'inline FILE "'.$fileBlock['Name'].'" "'.$fileBlock['Name']."\"\r\n");
+                }
+                fclose($handle);
+                break;
+        }
+    }
+    
+}
+function ExtractFileFromAgent($agent,$file) { //$file is a filename or a block id.
+    if(is_int($file)) {
+    }
+}
+?>
