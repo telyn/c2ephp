e55eb5ad745f5defe8605eb099dc21f50e277c3c
nornalbion <nornalbion@c8db70c2-6952-0410-97aa-c93078ccbfcc>
2010-02-20 15:00:42 +0000

    Add CreatureArchiveBlock, which was missing from the last commit! Also add lilo.creature for testing :)

---
+++ agents/PRAY/CreaturesArchiveBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once(dirname(__FILE__).'/CreaturesArchiveBlock.php');
+require_once(dirname(__FILE__).'/PrayBlock.php');
+abstract class CreaturesArchiveBlock extends PrayBlock {
+	public function CreaturesArchiveBlock(&$prayfile,$name,$content,$flags,$type) {
+		parent::PrayBlock($prayfile,$name,$content,$flags,$type);
+		$this->Decompress();
+	}
+
+	public function Compile() {
+		$compiled  = $this->EncodeBlockHeader();
+		$compiled .= Archive($this->GetData());
+	}
+
+	private function Decompress() {
+			$content = $this->GetData();
+			if($content{0} == 'C') {
+				$content = DeArchive($content);
+				if($content !== false) {
+					$this->SetData($content);
+					return true;
+				}
+				echo 'Invalid CreaturesArchive';
+				return false;
+			}
+			print('Not decompressing.');
+	}
+
+}
+?>
