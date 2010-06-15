2821c636ef8f4e10deda184f81c739971eb4dcb7
Telyn Z <175827+telyn@users.noreply.github.com>
2010-06-15 17:39:40 +0100

    Add documentation to all COB classes and a minor enhancement to COBUnknownBlock

--- agents/COB/AuthorBlock.php
+++ agents/COB/AuthorBlock.php
@@ -1,6 +1,7 @@
 <?php
 require_once(dirname(__FILE__).'/COBBlock.php');
 
+/// \brief Defines Author information about the COB
 class COBAuthorBlock extends COBBlock {
 	private $creationTime;
 	private $version;
@@ -10,6 +11,15 @@ class COBAuthorBlock extends COBBlock {
 	private $authorURL;
 	private $authorComments;
 	
+	/** \brief Creates a new COBAuthorBlock
+	 * \param $authorName the name of the author of this COB
+	 * \param $authorEmail the author's email address
+	 * \param $authorURL The author's website address
+	 * \param $authorComments Any comments the author had about this COB
+	 * \param $creationTime the time this COB was compiled
+	 * \param $version The version number of this COB
+	 * \param $revision the COB's revision number
+	 */
 	public function COBAuthorBlock($authorName,$authorEmail,$authorURL,$authorComments,$creationTime,$version,$revision) {
 		parent::COBBlock(COB_BLOCK_AUTHOR);
 		$this->authorName = $authorName;
@@ -20,29 +30,37 @@ class COBAuthorBlock extends COBBlock {
 		$this->version = $version;
 		$this->revision = $revision;
 	}
-	
+	/// \brief Gets the name of the author
 	public function GetAuthorName() {
 		return $this->authorName;
 	}
+	/// \brief Gets the author's email address
 	public function GetAuthorEmail() {
 		return $this->authorEmail;
 	}
+	/// \brief Gets the author's web address
 	public function GetAuthorURL() {
 		return $this->authorURL;
 	}
+	/// \brief Gets comments from the author
 	public function GetAuthorComments() {
 		return $this->authorComments;
 	}
+	/// \brief Gets the time this COB was created
 	public function GetCreationTime() {
 		return $this->creationTime;
 	}
+	/// \brief Gets the COB's version number
 	public function GetVersion() {
 		return $this->version;
 	}
+	/// \brief Gets the COB's revision
 	public function GetRevision() {
 		return $this->revision;
 	}
-	
+	/** Creates the COBAuthorBlock from an IReader
+	 * \param IReader $reader The IReader, currently at the position of the author block  
+	 */
 	public static function CreateFromReader(IReader $reader) {
 		$creationDay = $reader->ReadInt(1);
 		$creationMonth = $reader->ReadInt(1);
