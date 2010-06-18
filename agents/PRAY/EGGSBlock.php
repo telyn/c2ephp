7da33680678a7ba1b50a355a0d4044d29a36e59c
Telyn Z <175827+telyn@users.noreply.github.com>
2010-06-18 22:43:58 +0100

    Added basic convenience functions to EGGSBlock. EXCUSE: I'm feeling exceedingly lazy, been learning how to do Rails all day.

--- agents/PRAY/EGGSBlock.php
+++ agents/PRAY/EGGSBlock.php
@@ -6,5 +6,31 @@ class EGGSBlock extends TagBlock {
 		parent::TagBlock($prayfile,$name,$content,$flags,PRAY_BLOCK_EGGS);
 
 	}
+
+	public function GetAgentType() {
+		return $this->GetTag('Agent Type');
+	}
+	public function GetDependencyCount() {
+		return $this->GetTag('Dependency Count');
+	}
+	public function GetEggAnimationString() {
+		return $this->GetTag('Egg Animation String');
+	}
+	public function GetEggGalleryFemale() {
+		return $this->GetTag('Egg Gallery female');
+	}
+	public function GetEggGalleryMale() {
+		return $this->GetTag('Egg Gallery male');
+	}
+	public function GetEggGlyphFile1() {
+		return $this->GetTag('Egg Glyph File');
+	}
+	public function GetEggGlyphFile2() {
+		return $this->GetTag('Egg Glyph File 2');
+	}
+	public function GetGeneticsFile() {
+		return $this->GetTag('Genetics File');
+	}
+
 }
 ?>
