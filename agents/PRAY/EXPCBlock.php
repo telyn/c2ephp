7c4b6fdc9c7d265b2c54051b1a976691871c1387
Norn Albion <nornalbion@gmail.com>
2010-06-20 22:48:25 +0100

    Added documentation to EXPCBlock

--- agents/PRAY/EXPCBlock.php
+++ agents/PRAY/EXPCBlock.php
@@ -1,43 +1,83 @@
 <?php
 
 require_once(dirname(__FILE__).'/TagBlock.php');
+/** \brief Defines the class for EXPC (Creatures 3 exported creatures)
+ */
 class EXPCBlock extends TagBlock {
+	/** \brief Creates a new EXPCBlock
+	 * \param $prayfile The prayfile this block is contained in. Can be null.
+	 * \param $name The name of the block. Cannot be null.
+	 * \param $content The binary data this block contains. Can be null.
+	 * \param $flags The flags relating to this block. Should be zero or real flags.
+	 */
 	public function EXPCBlock($prayfile,$name,$content,$flags) {
 		parent::TagBlock($prayfile,$name,$content,$flags,PRAY_BLOCK_EXPC);
 	}
+	/** \brief Gets the age of the creature
+	 * \return The creature's age in ticks
+	 */
 	public function GetAgeInTicks() {
 		return $this->GetTag('Creature Age In Ticks');
 	}
+	/** \brief Gets the life stage of the creature
+	 * \return The life stage of the creature.
+	 */
 	public function GetLifeStage() {
 		return $this->GetTag('Creature Life Stage');
 	}
+	/** \brief Gets the time the creature was exported
+	 * \return The time the creature was exported as a UNIX timestamp. (Seconds since 00:00 1st Jan 1970)
+	 */
 	public function GetExportUNIXTime() {
 		return $this->GetTag('Exported At Real Time');
 	}
+	/** \brief Gets the world-time when the creature was exported
+	 * \return The age of the world, in ticks, when the creature was exported.
+	 */
 	public function GetExportWorldTime() {
 		return $this->GetTag('Exported At World Time');
 	}
+	/** \brief Gets the gender of the creature
+	 * 1 = male
+	 * 2 = female
+	 */
 	public function GetGender() {
 		return $this->GetTag('Gender');
 	}
+	/** \brief Gets the genus of the creature
+	 * I.e. whether it's a norn, grendel, ettin, or geat.
+	 * Most likely in that order. I think 1 is Norn.
+	 */
 	public function GetGenus() {
 		return $this->GetTag('Genus');
 	}
+	/** \brief Gets whether the creature is pregnant
+	 * 0 = not pregnant, 1 = pregnant.
+	 */
 	public function GetPregnancyStatus() {
 		return $this->GetTag('Pregnancy Status');
 	}
+	/** \brief Gets the variant (breed) of the creature
+	 * 
+	 */
 	public function GetVariant() {
 		return $this->GetTag('Variant');
 	}
+	/** \brief Gets the creature's name */
 	public function GetCreatureName() {
 		return $this->GetTag('Creature Name');
 	}
+	/** \brief Gets the name of the world the creature was exported from */
 	public function GetWorldName() {
 		return $this->GetTag('Exported From World Name');
 	}
+	/** \brief Gets the UID of the world the creature was exported from */
 	public function GetWorldUID() {
 		return $this->GetTag('Exported From World UID');
 	}
+	/** \brief Gets the gallery for the creature's head sprites.
+	 * This does not include the file extension.
+	 */
 	public function GetHeadGallery() {
 		return $this->GetTag('Head Gallery');
 	}
