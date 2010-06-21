c2b7808ce08e22c04539f876a985df4a9e96cfd0
Telyn Z <175827+telyn@users.noreply.github.com>
2010-06-21 22:07:01 +0100

    Added documentation to FILEBlock

--- agents/PRAY/FILEBlock.php
+++ agents/PRAY/FILEBlock.php
@@ -1,15 +1,43 @@
 <?php
 
 require_once(dirname(__FILE__).'/PrayBlock.php');
+/** \brief Class for a FILE block.
+ * Used mainly in agent files, a FILE block can contain any type of data
+ * These blocks are primarily used for sprite, body data and genetics files.
+ * You can only tell which type of file it is by looking at the file's name
+ **/
 class FILEBlock extends PrayBlock {
+  /** \brief Creates a new FILEBlock
+    * \param $prayfile The PRAYFile this FILEBlock belongs to. Can be null.
+    * \param $name The name of this file block (also the file's name)
+    * \param $content The binary data of this file block.
+    * \param $flags The block's flags. See PrayBlock.
+   **/
 	public function FILEBlock($prayfile,$name,$content,$flags) {
 		parent::PrayBlock($prayfile,$name,$content,$flags,PRAY_BLOCK_FILE);
 	}
+	/** \brief Compiles the block's data
+	 * This function is for compatibility with PrayBlock's abstract class
+	 * and allows FILEBlocks to be "compiled" and "decompiled"
+	 **/
 	protected function CompileBlockData() {
 		return $this->GetData();
 	}
+	/** \brief Decompiles the block's data
+	 * In other blocks, this function would convert the binary data from the
+	 * block into useful data. In FILEBlocks, this is impractical because
+	 * the binary data could be in any c2e file format.
+   **/
 	protected function DecompileBlockData() {
 		throw new Exception('It\'s impossible to decode a FILE.');
+	}
+	/** \brief Gets the name of the file */
+	public function GetFileName() {
+	   return $this->GetName();
+	}
+	/** \brief Gets the contents of the file */
+	public function GetFileContents() {
+	   return $this->GetData();
 	}
 }
 ?>
