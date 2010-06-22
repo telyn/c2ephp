5e376bcf5e33681c27b4142a7b27bb8b50f95bb9
Telyn Z <175827+telyn@users.noreply.github.com>
2010-06-22 23:25:19 +0100

    Added documentation to the (currently unfinished) GENEBlock

--- agents/PRAY/GENEBlock.php
+++ agents/PRAY/GENEBlock.php
@@ -1,14 +1,32 @@
 <?php
 
 require_once(dirname(__FILE__).'/PrayBlock.php');
+/** \brief The class for a GENE block 
+  * The contents are identical to that of a .gen file
+  * This class isn't done yet
+  */
 class GENEBlock extends PrayBlock {
+	/** \brief Creates a new GENEBlock
+      * \param $prayfile The PRAYFile object this block belongs to. Can be null.
+      * \param $name The block's name. This is a moniker, possibly ending in .gen. TODO: Does it end in gen?
+      * \param $content The block's binary data. Used when constructing from a PrayFile
+      * \param $flags The block's flags, which apply to the binary data as-is.
+      */
 	public function GENEBlock($prayfile,$name,$content,$flags) {
 		parent::PrayBlock($prayfile,$name,$content,$flags,PRAY_BLOCK_GENE);
 
 	}
-	public function CompileBlockData() {
+	/** \brief Compiles the block's data
+      * Does nothing really, just pipes the original binary data along
+      * TODO: Make code for creation of GENE blocks.
+      */
+	protected function CompileBlockData() {
 		return $this->GetData();
 	}
+	/** \brief Decompiles the block's data
+      * Called automatically when necessary.
+      * TODO: This currently does nothing as I don't know how to decomile a gene file.
+      */
 	protected function DecompileBlockData() {
 		throw new Exception('I don\'t know how to decompile a GENE.');
 	}
