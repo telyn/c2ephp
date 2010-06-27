6e8b1361afcf174348bf9b5d03981866af883229
Norn Albion <nornalbion@gmail.com>
2010-06-27 23:13:37 +0100

    Added documentation to C16File. I think I'll add write support for C16s tomorrow.

--- sprites/C16File.php
+++ sprites/C16File.php
@@ -2,12 +2,22 @@
 require_once(dirname(__FILE__).'/../support/IReader.php');
 require_once(dirname(__FILE__).'/ISpriteFile.php');
 require_once(dirname(__FILE__).'/C16Frame.php');
+
+/** \brief Class representing a C16 sprite file. 
+  * This documentation was written in the future, when the code had already been written
+  * In other words, what is true according to what's in the docs is not what's actually true RIGHT NOW
+  * I will remove this notice when I finish coding this class.
+  */
 class C16File implements ISpriteFile
 {
 	private $encoding;
 	private $frameCount;
 	private $frames;
 	private $reader;
+	/** \brief Creates a new C16File object.
+	  * If $reader is null, creates an empty C16File ready to add sprites to.
+	  * \param IReader $reader The reader to read the sprites from. Will be able to be null
+	  */
 	public function C16File(IReader $reader)
 	{
 		$this->reader = $reader;
@@ -27,16 +37,23 @@ class C16File implements ISpriteFile
 			$this->frames[$x] = new C16Frame($this->reader,$this->encoding);
 		} 
 	}
+	/** \brief Gets the specified C16Frame
+	  * \param $frame The index of the frame to get.
+	  * \return A C16Frame
+	  */
 	public function GetFrame($frame) {
 		if($this->frameCount < ($frame+1))
-			throw new Exception('OutputPNG - Frame out of bounds - '.$frame);
+			throw new Exception('GetFrame - Frame out of bounds - '.$frame);
 		return $this->frames[$frame];
 	}
+	/** \brief Makes a PNG of the specified frame
+	  * This function is syntactic sugar for GetFrame($frame)->OutputPNG();
+	  * \param $frame The frame to output.
+	  * \return A PNG as a binary string.
+	  */
 	public function OutputPNG($frame)
 	{
-		if($this->frameCount < ($frame+1))
-			throw new Exception('OutputPNG - Frame out of bounds - '.$frame);
-		return $this->frames[$frame]->OutputPNG($this->encoding);
+		$this->GetFrame($frame)->OutputPNG($this->encoding);
 	}
 }
 ?>
