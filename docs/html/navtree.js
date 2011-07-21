8dd69d19a8fd355c128bd1081d87fe4de6771994
Norn Albion <norn@nornalbion.com>
2011-07-21 19:44:25 +0100

    Updated docs

---
+++ docs/html/navtree.js
@@ -0,0 +1,422 @@
+var NAVTREE =
+[
+  [ "c2ephp", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "AGNTBlock", "classAGNTBlock.html", null ],
+      [ "BinaryBlock", "classBinaryBlock.html", null ],
+      [ "C16File", "classC16File.html", null ],
+      [ "C16Frame", "classC16Frame.html", null ],
+      [ "CAOSHighlighter", "classCAOSHighlighter.html", null ],
+      [ "COB", "classCOB.html", null ],
+      [ "COBAgentBlock", "classCOBAgentBlock.html", null ],
+      [ "COBAuthorBlock", "classCOBAuthorBlock.html", null ],
+      [ "COBBlock", "classCOBBlock.html", null ],
+      [ "COBDependency", "classCOBDependency.html", null ],
+      [ "COBFileBlock", "classCOBFileBlock.html", null ],
+      [ "COBUnknownBlock", "classCOBUnknownBlock.html", null ],
+      [ "CREABlock", "classCREABlock.html", null ],
+      [ "CreatureHistory", "classCreatureHistory.html", null ],
+      [ "CreatureHistoryEvent", "classCreatureHistoryEvent.html", null ],
+      [ "CreaturesArchiveBlock", "classCreaturesArchiveBlock.html", null ],
+      [ "DFAMBlock", "classDFAMBlock.html", null ],
+      [ "DSAGBlock", "classDSAGBlock.html", null ],
+      [ "DSEXBlock", "classDSEXBlock.html", null ],
+      [ "EGGSBlock", "classEGGSBlock.html", null ],
+      [ "EXPCBlock", "classEXPCBlock.html", null ],
+      [ "FILEBlock", "classFILEBlock.html", null ],
+      [ "FileReader", "classFileReader.html", null ],
+      [ "GENEBlock", "classGENEBlock.html", null ],
+      [ "GLSTBlock", "classGLSTBlock.html", null ],
+      [ "IReader", "interfaceIReader.html", null ],
+      [ "LIVEBlock", "classLIVEBlock.html", null ],
+      [ "PHOTBlock", "classPHOTBlock.html", null ],
+      [ "PrayBlock", "classPrayBlock.html", null ],
+      [ "PrayDependency", "classPrayDependency.html", null ],
+      [ "PRAYFile", "classPRAYFile.html", null ],
+      [ "S16File", "classS16File.html", null ],
+      [ "S16Frame", "classS16Frame.html", null ],
+      [ "SFAMBlock", "classSFAMBlock.html", null ],
+      [ "SPRFile", "classSPRFile.html", null ],
+      [ "SPRFrame", "classSPRFrame.html", null ],
+      [ "SpriteFile", "classSpriteFile.html", null ],
+      [ "SpriteFrame", "classSpriteFrame.html", null ],
+      [ "StringReader", "classStringReader.html", null ],
+      [ "TagBlock", "classTagBlock.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "CAOSHighlighter", "classCAOSHighlighter.html", null ],
+      [ "COB", "classCOB.html", null ],
+      [ "COBBlock", "classCOBBlock.html", [
+        [ "COBAgentBlock", "classCOBAgentBlock.html", null ],
+        [ "COBAuthorBlock", "classCOBAuthorBlock.html", null ],
+        [ "COBFileBlock", "classCOBFileBlock.html", null ],
+        [ "COBUnknownBlock", "classCOBUnknownBlock.html", null ]
+      ] ],
+      [ "COBDependency", "classCOBDependency.html", null ],
+      [ "CreatureHistory", "classCreatureHistory.html", null ],
+      [ "CreatureHistoryEvent", "classCreatureHistoryEvent.html", null ],
+      [ "IReader", "interfaceIReader.html", [
+        [ "FileReader", "classFileReader.html", null ],
+        [ "StringReader", "classStringReader.html", null ]
+      ] ],
+      [ "PrayBlock", "classPrayBlock.html", [
+        [ "BinaryBlock", "classBinaryBlock.html", null ],
+        [ "CreaturesArchiveBlock", "classCreaturesArchiveBlock.html", [
+          [ "CREABlock", "classCREABlock.html", null ],
+          [ "GLSTBlock", "classGLSTBlock.html", null ]
+        ] ],
+        [ "FILEBlock", "classFILEBlock.html", null ],
+        [ "GENEBlock", "classGENEBlock.html", null ],
+        [ "PHOTBlock", "classPHOTBlock.html", null ],
+        [ "TagBlock", "classTagBlock.html", [
+          [ "AGNTBlock", "classAGNTBlock.html", [
+            [ "DSAGBlock", "classDSAGBlock.html", null ],
+            [ "LIVEBlock", "classLIVEBlock.html", null ]
+          ] ],
+          [ "EGGSBlock", "classEGGSBlock.html", null ],
+          [ "EXPCBlock", "classEXPCBlock.html", [
+            [ "DSEXBlock", "classDSEXBlock.html", [
+              [ "DFAMBlock", "classDFAMBlock.html", null ]
+            ] ],
+            [ "SFAMBlock", "classSFAMBlock.html", null ]
+          ] ]
+        ] ]
+      ] ],
+      [ "PrayDependency", "classPrayDependency.html", null ],
+      [ "PRAYFile", "classPRAYFile.html", null ],
+      [ "SpriteFile", "classSpriteFile.html", [
+        [ "C16File", "classC16File.html", null ],
+        [ "S16File", "classS16File.html", null ],
+        [ "SPRFile", "classSPRFile.html", null ]
+      ] ],
+      [ "SpriteFrame", "classSpriteFrame.html", [
+        [ "C16Frame", "classC16Frame.html", null ],
+        [ "S16Frame", "classS16Frame.html", null ],
+        [ "SPRFrame", "classSPRFrame.html", null ]
+      ] ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "c2ephp.php", "c2ephp_8php.html", null ],
+      [ "agents/COB.php", "COB_8php.html", null ],
+      [ "agents/PRAYFile.php", "PRAYFile_8php.html", null ],
+      [ "agents/COB/AgentBlock.php", "AgentBlock_8php.html", null ],
+      [ "agents/COB/AuthorBlock.php", "AuthorBlock_8php.html", null ],
+      [ "agents/COB/COBBlock.php", "COBBlock_8php.html", null ],
+      [ "agents/COB/FileBlock.php", "FileBlock_8php.html", null ],
+      [ "agents/COB/UnknownBlock.php", "UnknownBlock_8php.html", null ],
+      [ "agents/CreatureHistory/CreatureHistory.php", "CreatureHistory_8php.html", null ],
+      [ "agents/CreatureHistory/CreatureHistoryEvent.php", "CreatureHistoryEvent_8php.html", null ],
+      [ "agents/PRAY/AGNTBlock.php", "AGNTBlock_8php.html", null ],
+      [ "agents/PRAY/BinaryBlock.php", "BinaryBlock_8php.html", null ],
+      [ "agents/PRAY/CREABlock.php", "CREABlock_8php.html", null ],
+      [ "agents/PRAY/CreaturesArchiveBlock.php", "CreaturesArchiveBlock_8php.html", null ],
+      [ "agents/PRAY/DFAMBlock.php", "DFAMBlock_8php.html", null ],
+      [ "agents/PRAY/DSAGBlock.php", "DSAGBlock_8php.html", null ],
+      [ "agents/PRAY/DSEXBlock.php", "DSEXBlock_8php.html", null ],
+      [ "agents/PRAY/EGGSBlock.php", "EGGSBlock_8php.html", null ],
+      [ "agents/PRAY/EXPCBlock.php", "EXPCBlock_8php.html", null ],
+      [ "agents/PRAY/FILEBlock.php", "FILEBlock_8php.html", null ],
+      [ "agents/PRAY/GENEBlock.php", "GENEBlock_8php.html", null ],
+      [ "agents/PRAY/GLSTBlock.php", "GLSTBlock_8php.html", null ],
+      [ "agents/PRAY/IAgentBlock.php", "IAgentBlock_8php.html", null ],
+      [ "agents/PRAY/LIVEBlock.php", "LIVEBlock_8php.html", null ],
+      [ "agents/PRAY/PHOTBlock.php", "PHOTBlock_8php.html", null ],
+      [ "agents/PRAY/PrayBlock.php", "PrayBlock_8php.html", null ],
+      [ "agents/PRAY/PrayDependency.php", "PrayDependency_8php.html", null ],
+      [ "agents/PRAY/SFAMBlock.php", "SFAMBlock_8php.html", null ],
+      [ "agents/PRAY/TagBlock.php", "TagBlock_8php.html", null ],
+      [ "caos/highlight/CAOSHighlighter.php", "CAOSHighlighter_8php.html", null ],
+      [ "caos/highlight/C2/Commands.php", "C2_2Commands_8php.html", null ],
+      [ "caos/highlight/C2/CommandVariables.php", "C2_2CommandVariables_8php.html", null ],
+      [ "caos/highlight/C2/FlowControls.php", "C2_2FlowControls_8php.html", null ],
+      [ "caos/highlight/C2/Operators.php", "C2_2Operators_8php.html", null ],
+      [ "caos/highlight/C2/Variables.php", "C2_2Variables_8php.html", null ],
+      [ "caos/highlight/C3/Commands.php", "C3_2Commands_8php.html", null ],
+      [ "caos/highlight/C3/CommandVariables.php", "C3_2CommandVariables_8php.html", null ],
+      [ "caos/highlight/C3/FlowControls.php", "C3_2FlowControls_8php.html", null ],
+      [ "caos/highlight/C3/Operators.php", "C3_2Operators_8php.html", null ],
+      [ "caos/highlight/C3/Variables.php", "C3_2Variables_8php.html", null ],
+      [ "caos/highlight/DS/Commands.php", "DS_2Commands_8php.html", null ],
+      [ "caos/highlight/DS/CommandVariables.php", "DS_2CommandVariables_8php.html", null ],
+      [ "caos/highlight/DS/FlowControls.php", "DS_2FlowControls_8php.html", null ],
+      [ "caos/highlight/DS/Operators.php", "DS_2Operators_8php.html", null ],
+      [ "caos/highlight/DS/Variables.php", "DS_2Variables_8php.html", null ],
+      [ "sprites/C16File.php", "C16File_8php.html", null ],
+      [ "sprites/C16Frame.php", "C16Frame_8php.html", null ],
+      [ "sprites/S16File.php", "S16File_8php.html", null ],
+      [ "sprites/S16Frame.php", "S16Frame_8php.html", null ],
+      [ "sprites/SPRFile.php", "SPRFile_8php.html", null ],
+      [ "sprites/SPRFrame.php", "SPRFrame_8php.html", null ],
+      [ "sprites/SpriteFile.php", "SpriteFile_8php.html", null ],
+      [ "sprites/SpriteFrame.php", "SpriteFrame_8php.html", null ],
+      [ "support/Archiver.php", "Archiver_8php.html", null ],
+      [ "support/FileReader.php", "FileReader_8php.html", null ],
+      [ "support/IReader.php", "IReader_8php.html", null ],
+      [ "support/StringReader.php", "StringReader_8php.html", null ],
+      [ "tests/caos-highlight.php", "caos-highlight_8php.html", null ],
+      [ "tests/carchive.php", "carchive_8php.html", null ],
+      [ "tests/cob-extract.php", "cob-extract_8php.html", null ],
+      [ "tests/cob-thumbnail.php", "cob-thumbnail_8php.html", null ],
+      [ "tests/cob.php", "cob_8php.html", null ],
+      [ "tests/pray-agnt-animation.php", "pray-agnt-animation_8php.html", null ],
+      [ "tests/pray-extract.php", "pray-extract_8php.html", null ],
+      [ "tests/pray-frankenstein.php", "pray-frankenstein_8php.html", null ],
+      [ "tests/pray-glst-make.php", "pray-glst-make_8php.html", null ],
+      [ "tests/pray-glst-phot.php", "pray-glst-phot_8php.html", null ],
+      [ "tests/pray-glst.php", "pray-glst_8php.html", null ],
+      [ "tests/pray.php", "pray_8php.html", null ],
+      [ "tests/sprite-extract.php", "sprite-extract_8php.html", null ],
+      [ "tests/sprite-recompile.php", "sprite-recompile_8php.html", null ],
+      [ "tests/sprite.php", "sprite_8php.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
