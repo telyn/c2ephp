2f8e725174c60798de22a6e9077c45b62f2c5340
Norn Albion <norn@nornalbion.com>
2011-07-21 17:39:48 +0100

    Removed unfinished test in ReaderTest

--- unit-tests/ReaderTest.php
+++ unit-tests/ReaderTest.php
@@ -6,28 +6,6 @@ require_once(dirname(__FILE__) . '/../support/StringReader.php');
 class ReaderTest extends c2ephpAbstractTestCase {
 
     /**
-     * @dataProvider createPRAYFiles
-     */
-    public function testFileReader($prayFile, $info) {
-
-        $fileReader = new FileReader($info['path']);
-
-        $this->assertEquals($info['byte0'],$fileReader->Read(1));
-        $this->assertEquals($info['byte1'],$fileReader->Read(1)); // tests position increments
-        // now at position 2, skip 2 = position 4
-        $fileReader->skip(2);
-        $this->assertEquals($info['byte4'],$fileReader->Read(1)); // tests skip skips the correct amount
-        $fileReader->seek(0);
-        $this->assertEquals($info['byte0'],$fileReader->Read(1)); // tests seek works
-        $this->assertEquals($info['substring'],$fileReader->GetSubString(3,8)); // tests getsubstring gets the right substring
-        $this->assertEquals($info['byte1'],$fileReader->Read(1)); // tests that getsubstring leaves cursor where it found it
-
-        $this->assertEquals($info['length'],strlen($fileReader->GetSubString(0)));
-        $this->assertEquals($info['md5sum'],md5($fileReader->GetSubString(0)));
-
-    }
-    
-    /**
      * @dataProvider generateStrings
      */
     public function testStringReader($string) {
